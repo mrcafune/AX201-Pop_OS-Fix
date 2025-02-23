7dcd63d121d1bd8732c24f1cd52d4554d5a1f37a
B Cafune <101951803+mrcafune@users.noreply.github.com>
2025-02-23 12:17:30 -0300

    Update ax201.sh
    Changed order of text and clarified

--- ax201.sh
+++ ax201.sh
@@ -6,14 +6,16 @@ echo "Updating system and reinstalling firmware..."
 apt update
 apt install --reinstall linux-firmware -y
 update-initramfs -u
+
 echo "Ensure the following in your BIOS settings:"
 echo "- Secure Boot is DISABLED"
 echo "- Fast Boot is DISABLED"
 echo "- WiFi or wireless network devices are ENABLED"
-read -p "Press Enter to continue after verifying BIOS settings..."
+read -p "Press Enter to if this is the case, otherwise CTRL-C and return to this"
+
 echo "Installing the mainline kernel tool..."
 apt install mainline -y
 echo "Open 'mainline' and ensure you're on the latest kernel if issues persist."
 echo "You can install and reboot to test the latest kernel using the mainline tool."
-echo "Rebooting to apply changes"
+echo -p "Press enter to Reboot to apply changes, otherwise CTRL-C"
 reboot
